b9d313fa46f1b9a813c2d7c49067daaf8b21344a
Adrian Fernandez <adrian.fernandez@zinklar.com>
2022-09-02 12:32:22 +0200

    test: add unit test to get initial positions and direction

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,25 @@
+import { render } from "@testing-library/react"
 import { describe } from "vitest"
+import App from "./App"
 
-describe('Ee', () => {
-  it('', ()=> {
-    expect(1).toEqual(1)
+describe('App', () => {
+  it('Should show the initial position', () => {
+    const { getByText } = render(<App x={0} y={0}/>)
+
+    expect(getByText('x=0, y=0')).toBeInTheDocument()
+  })
+
+  it('Should show the initial direction', () => {
+    const { getByText } = render(<App x={0} y={0}/>)
+
+    expect(getByText('direction=N')).toBeInTheDocument()
+  })
+
+  it('Should show the initial position recieved', () => {
+    const x = 1
+    const y = 1
+    const { getByText } = render(<App x={x} y={y}/>)
+
+    expect(getByText('x=1, y=1')).toBeInTheDocument()
   })
 })
